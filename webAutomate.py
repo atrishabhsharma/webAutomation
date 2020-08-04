9368d14a01828573f0579c320bc2d30d4b683dc0
atrishabhsharma <atrishabh1999@gmail.com>
2020-08-04 22:18:12 +0530

    first code for web automation more to come

---
+++ webAutomate.py
@@ -0,0 +1,14 @@
+from selenium import webdriver
+
+driver = webdriver.Chrome()
+driver.get('https://youtube.com/')
+
+
+
+searchbox = driver.find_element_by_xpath('//*[@id="search"]')
+searchbox.send_keys('sadhguru')
+
+
+searchButton = driver.find_element_by_xpath('//*[@id="search-icon-legacy"]')
+
+searchButton.click()
